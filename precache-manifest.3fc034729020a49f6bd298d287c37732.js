8adbf7a621986551f05f49665f079090f8128a87
Jon Fox <jonfuchs45@gmail.com>
2019-03-22 17:58:26 -0600

    Updates

---
+++ precache-manifest.3fc034729020a49f6bd298d287c37732.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "0763f2492d17c139e43c5bcea423d9c7",
+    "url": "/static/media/moodle-original.0763f249.svg"
+  },
+  {
+    "revision": "112d06b27bef60a98756",
+    "url": "/static/css/main.476e6de3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cb01a0fe1b61ec19398",
+    "url": "/static/js/2.e861508f.chunk.js"
+  },
+  {
+    "revision": "b7d3f6cee023056b336b1668a9f2b4cb",
+    "url": "/static/media/icon-github.b7d3f6ce.svg"
+  },
+  {
+    "revision": "53147c1479e66ce2f3091d3f4048476c",
+    "url": "/static/media/icon-linkedin.53147c14.svg"
+  },
+  {
+    "revision": "2494494a2068e6abd031ae081b0f0019",
+    "url": "/static/media/jfwd.2494494a.svg"
+  },
+  {
+    "revision": "b9a60c22bca43ec579e91f0b85a86dba",
+    "url": "/static/media/Teach-Tools desktop game display.b9a60c22.png"
+  },
+  {
+    "revision": "9c3205821b60307ecd4d7d4fc57b44fa",
+    "url": "/static/media/jon_fam-pic.9c320582.jpg"
+  },
+  {
+    "revision": "2ca884530d6e08f8540700e76c3a63cb",
+    "url": "/static/media/css3-original-wordmark.2ca88453.svg"
+  },
+  {
+    "revision": "eb0bfe64ddcaa0b4c3be19b19541b584",
+    "url": "/static/media/html5-original-wordmark.eb0bfe64.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/static/media/javascript-original.de79c8f1.svg"
+  },
+  {
+    "revision": "fadd9c9566ccc46c6b0e73caca5860de",
+    "url": "/static/media/react-original-wordmark.fadd9c95.svg"
+  },
+  {
+    "revision": "b7dc3b99382465652bda0e61515c0937",
+    "url": "/static/media/redux-original.b7dc3b99.svg"
+  },
+  {
+    "revision": "f13b681bf07a1d23ee3fd1c591385ce9",
+    "url": "/static/media/nodejs-original-wordmark.f13b681b.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/static/media/sass-original.88c10477.svg"
+  },
+  {
+    "revision": "9474528ec861c5d35b9efd498ddef326",
+    "url": "/static/media/postgresql-original-wordmark.9474528e.svg"
+  },
+  {
+    "revision": "e83e60b310396562d703c9ccff616b91",
+    "url": "/static/media/npm-original-wordmark.e83e60b3.svg"
+  },
+  {
+    "revision": "f1a0b0e9c7eb8a3294e84f11441de5df",
+    "url": "/static/media/express-original-wordmark.f1a0b0e9.svg"
+  },
+  {
+    "revision": "805052423abf5e37804e11b9f7fff41e",
+    "url": "/static/media/figma-1.80505242.svg"
+  },
+  {
+    "revision": "112d06b27bef60a98756",
+    "url": "/static/js/main.d50088c4.chunk.js"
+  },
+  {
+    "revision": "b573a7344fb4fed97e4d8669fbaf04ae",
+    "url": "/static/media/CYN desktop home page.b573a734.gif"
+  },
+  {
+    "revision": "676cfcf2c4793e04817ac8923f3b7110",
+    "url": "/static/media/CYN desktop home page.676cfcf2.png"
+  },
+  {
+    "revision": "0c2d93e1cb8f6ea56cf4deef1be3d04e",
+    "url": "/static/media/CYN desktop HS event page.0c2d93e1.png"
+  },
+  {
+    "revision": "e33d595ff262ce722b4bafb08f8f24fc",
+    "url": "/static/media/CYN desktop news page.e33d595f.png"
+  },
+  {
+    "revision": "8e37270f36ce54dbdc9e6e6a2f289372",
+    "url": "/static/media/CYN desktop rsvp page.8e37270f.png"
+  },
+  {
+    "revision": "b5ce502092238e84b108da60af68b3fe",
+    "url": "/static/media/CYN mobile home page.b5ce5020.png"
+  },
+  {
+    "revision": "45b6019563b11b802e50309a12a8a59f",
+    "url": "/static/media/CYN mobile HS event page.45b60195.png"
+  },
+  {
+    "revision": "03dcfa16062df9438252a79c058eb2b0",
+    "url": "/static/media/CYN mobile rsvp page.03dcfa16.png"
+  },
+  {
+    "revision": "7a47d03299f2e66ccd896ff8525b5e2f",
+    "url": "/static/media/CYN tablet home page.7a47d032.png"
+  },
+  {
+    "revision": "96023ddec6521b6078c29be5585fcb23",
+    "url": "/static/media/CYN tablet rsvp page.96023dde.png"
+  },
+  {
+    "revision": "6fbc8e0e45e7666302df094f6efaa51b",
+    "url": "/static/media/Teach-Tools game demo.6fbc8e0e.mp4"
+  },
+  {
+    "revision": "56e1b03dad664bf797960533e6e97e15",
+    "url": "/static/media/Teach-Tools desktop home page.56e1b03d.png"
+  },
+  {
+    "revision": "08bda9d616828d9d23d702f1339a065b",
+    "url": "/static/media/Teach-Tools mobile home page.08bda9d6.png"
+  },
+  {
+    "revision": "351e925b1029c8a9fda66caa89062343",
+    "url": "/static/media/Teach-Tools tablet game control.351e925b.png"
+  },
+  {
+    "revision": "26d78405550c4d2e27731b9d67ac75cb",
+    "url": "/static/media/round-phone_iphone-24px.26d78405.svg"
+  },
+  {
+    "revision": "f83b2d94a05ba3bad2a10b80816f8001",
+    "url": "/static/media/round-mail_outline-24px.f83b2d94.svg"
+  },
+  {
+    "revision": "41139de398802030ba37ea60734c69ec",
+    "url": "/static/media/round-home-24px.41139de3.svg"
+  },
+  {
+    "revision": "50950c868e71165616fc1b49578765bd",
+    "url": "/static/media/birds-black-and-white-black-and-white-1229102.50950c86.jpg"
+  },
+  {
+    "revision": "9801950b34f102f4cfc5f917c231880c",
+    "url": "/index.html"
+  }
+];
